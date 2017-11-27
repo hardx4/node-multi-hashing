5110742e3f5938f5403890284c221df11b008fb5
hardx4 <felipe_f0@hotmail.com>
2017-11-27 01:42:46 -0400

    New algo code try 1

--- timetravel10.c
+++ timetravel10.c
@@ -6,171 +6,94 @@
 
 #include "sha3/sph_blake.h"
 #include "sha3/sph_bmw.h"
+#include "sha3/sph_groestl.h"
 #include "sha3/sph_jh.h"
 #include "sha3/sph_keccak.h"
 #include "sha3/sph_skein.h"
-#include "algo/luffa/sse2/luffa_for_sse2.h"
-#include "algo/cubehash/sse2/cubehash_sse2.h"
+#include "sha3/sph_luffa.h"
+#include "sha3/sph_cubehash.h"
 #include "sha3/sph_shavite.h"
-#include "algo/simd/sse2/nist.h"
-#include "sha3/sph_groestl.h"
+#include "sha3/sph_simd.h"
+#include "sha3/sph_echo.h"
 
-#define HASH_FUNC_BASE_TIMESTAMP 1492973331U // BitCore: Genesis Timestamp
-#define HASH_FUNC_COUNT 10
-#define HASH_FUNC_COUNT_PERMUTATIONS 40320
+#define HASH_FUNC_BASE_TIMESTAMP 1492973331 // BitCore: Genesis Timestamp
+#define HASH_FUNC_COUNT 10                  // BitCore: HASH_FUNC_COUNT of 11
+#define HASH_FUNC_COUNT_PERMUTATIONS 40320 // BitCore: HASH_FUNC_COUNT! 
 
-#define _ALIGN(x) __attribute__ ((aligned(x)))
-
-// helpers
-inline void swap(int *a, int *b) {
-	int c = *a;
-	*a = *b;
-	*b = c;
-}
-
-static void reverse(int *pbegin, int *pend) {
-	while ((pbegin != pend) && (pbegin != --pend))
-	{
-		swap(pbegin, pend);
-		pbegin++;
-	}
-}
-
-static void next_permutation(int *pbegin, int *pend) {
-	if (pbegin == pend)
-		return;
-
-	int *i = pbegin;
-	++i;
-	if (i == pend)
-		return;
-
-	i = pend;
-	--i;
-
-	while (1) {
-		int *j = i;
-		--i;
-
-		if (*i < *j) {
-			int *k = pend;
-
-			while (!(*i < *--k))
-				/* pass */;
-
-			swap(i, k);
-			reverse(j, pend);
-			return; // true
-		}
-
-		if (i == pbegin) {
-			reverse(pbegin, pend);
-			return; // false
-		}
-	}
-}
-// helpers
+extern uint32_t permutations[];
 
 void timetravel10_hash(const char* input, char* output, uint32_t len)
 {
-	uint32_t hash[16 * HASH_FUNC_COUNT] __attribute__((aligned(64)));
-	uint32_t *hashA, *hashB;
-	uint32_t dataLen = 64;
-	uint32_t *work_data = (uint32_t *)input;
+	uint32_t hash[16], i;
+	uint32_t time = ((uint32_t *)input)[17];
+	uint32_t permutation = permutations[(time - HASH_FUNC_BASE_TIMESTAMP) % HASH_FUNC_COUNT_PERMUTATIONS];
 
-	const uint32_t timestamp = work_data[17];
+	sph_blake512_context	ctx_blake;
+	sph_bmw512_context		ctx_bmw;
+	sph_groestl512_context	ctx_groestl;
+	sph_skein512_context	ctx_skein;
+	sph_jh512_context		ctx_jh;
+	sph_keccak512_context	ctx_keccak;
+	sph_luffa512_context	ctx_luffa;
+	sph_cubehash512_context	ctx_cubehash;
+	sph_shavite512_context	ctx_shavite;
+	sph_simd512_context		ctx_simd;
 
-	const int midlen = 64;          // bytes
-	const int tail = 80 - midlen;   // 16
+	memset(hash, 0, 16 * sizeof(uint32_t));
 
-	sph_blake512_context    ctx_blake;
-	sph_bmw512_context      ctx_bmw;
-	sph_skein512_context    ctx_skein;
-	sph_jh512_context       ctx_jh;
-	sph_keccak512_context   ctx_keccak;
-	hashState_luffa         ctx_luffa;
-	cubehashParam           ctx_cubehash;
-	sph_shavite512_context  ctx_shavite;
-	hashState_sd            ctx_simd;
-	sph_groestl512_context  ctx_groestl;
+	sph_blake512_init(&ctx_blake);
+	sph_blake512(&ctx_blake, input, 80);
+	sph_blake512_close(&ctx_blake, hash);
 
-	// We want to permute algorithms. To get started we
-	// initialize an array with a sorted sequence of unique
-	// integers where every integer represents its own algorithm.
-	uint32_t permutation[HASH_FUNC_COUNT];
-	for (uint32_t i = 0; i < HASH_FUNC_COUNT; i++) {
-		permutation[i] = i;
-	}
+	sph_bmw512_init(&ctx_bmw);
+	sph_bmw512(&ctx_bmw, hash, 64);
+	sph_bmw512_close(&ctx_bmw, hash);
 
-	// Compute the next permuation
-	uint32_t steps = (timestamp - HASH_FUNC_BASE_TIMESTAMP) % HASH_FUNC_COUNT_PERMUTATIONS;
-	for (uint32_t i = 0; i < steps; i++) {
-		next_permutation(permutation, permutation + HASH_FUNC_COUNT);
-	}
+	for (i = 0; i < (4 * (HASH_FUNC_COUNT - 2)); i += 4) {
+		switch ((permutation >> i) & 0xf) {
 
-	for (uint32_t i = 0; i < HASH_FUNC_COUNT; i++) {
-		if (i == 0) {
-			dataLen = len;
-			hashA = work_data;
-		}
-		else {
-			dataLen = 64;
-			hashA = &hash[16 * (i - 1)];
-		}
-		hashB = &hash[16 * i];
-
-		switch (permutation[i]) {
 		case 0:
-			sph_blake512_init(&ctx_blake);
-			sph_blake512(&ctx_blake, hashA, dataLen);
-			sph_blake512_close(&ctx_blake, hashB);
+			sph_groestl512_init(&ctx_groestl);
+			sph_groestl512(&ctx_groestl, hash, 64);
+			sph_groestl512_close(&ctx_groestl, hash);
 			break;
 		case 1:
-			sph_bmw512_init(&ctx_bmw);
-			sph_bmw512(&ctx_bmw, hashA, dataLen);
-			sph_bmw512_close(&ctx_bmw, hashB);
+			sph_skein512_init(&ctx_skein);
+			sph_skein512(&ctx_skein, hash, 64);
+			sph_skein512_close(&ctx_skein, hash);
 			break;
 		case 2:
-			sph_groestl512_init(&ctx_groestl);
-			sph_groestl512(&ctx_groestl, hashA, dataLen);
-			sph_groestl512_close(&ctx_groestl, hashB);
+			sph_jh512_init(&ctx_jh);
+			sph_jh512(&ctx_jh, hash, 64);
+			sph_jh512_close(&ctx_jh, hash);
 			break;
 		case 3:
-			sph_skein512_init(&ctx_skein);
-			sph_skein512(&ctx_skein, hashA, dataLen);
-			sph_skein512_close(&ctx_skein, hashB);
+			sph_keccak512_init(&ctx_keccak);
+			sph_keccak512(&ctx_keccak, hash, 64);
+			sph_keccak512_close(&ctx_keccak, hash);
 			break;
 		case 4:
-			sph_jh512_init(&ctx_jh);
-			sph_jh512(&ctx_jh, hashA, dataLen);
-			sph_jh512_close(&ctx_jh, hashB);
+			sph_luffa512_init(&ctx_luffa);
+			sph_luffa512(&ctx_luffa, hash, 64);
+			sph_luffa512_close(&ctx_luffa, hash);
 			break;
 		case 5:
-			sph_keccak512_init(&ctx_keccak);
-			sph_keccak512(&ctx_keccak, hashA, dataLen);
-			sph_keccak512_close(&ctx_keccak, hashB);
+			sph_cubehash512_init(&ctx_cubehash);
+			sph_cubehash512(&ctx_cubehash, hash, 64);
+			sph_cubehash512_close(&ctx_cubehash, hash);
 			break;
-		case 6:			
-			init_luffa(&ctx_luffa, 512);
-			update_and_final_luffa(&ctx_luffa, (BitSequence*)hashB, (const BitSequence *)hashA, dataLen);
-			break;
-		case 7:			
-			cubehashInit(&ctx_cubehash, 512, 16, 32);
-			cubehashUpdateDigest(&ctx_cubehash, (byte*)hashB, (const byte*)hashA, dataLen);
-			break;
-		case 8:
+		case 6:
 			sph_shavite512_init(&ctx_shavite);
-			sph_shavite512(&ctx_shavite, hashA, dataLen);
-			sph_shavite512_close(&ctx_shavite, hashB);
+			sph_shavite512(&ctx_shavite, hash, 64);
+			sph_shavite512_close(&ctx_shavite, hash);
 			break;
-		case 9:			
-			init_sd(&ctx_simd, 512);
-			update_final_sd(&ctx_simd, (BitSequence *)hashB, (const BitSequence *)hashA, dataLen * 8);
-			break;
-		default:
+		case 7:
+			sph_simd512_init(&ctx_simd);
+			sph_simd512(&ctx_simd, hash, 64);
+			sph_simd512_close(&ctx_simd, hash);
 			break;
 		}
 	}
 
-	memcpy(output, &hash[16 * (HASH_FUNC_COUNT - 1)], 32);
+	memcpy(output, hash, 32);
 }
