1b8feabed38dc0e830d0671fe9e4ad9ffe140bde
hardx4 <felipe_f0@hotmail.com>
2017-12-31 11:54:30 -0400

    Try X13 Bitcoin Diamond #4

--- x13sm3.c
+++ x13sm3.c
@@ -22,9 +22,7 @@
 
 void x13sm3_hash(const char* input, char* output, uint32_t len)
 {
-	unsigned char hash[128];
-
-	memset(hash, 0, 128);
+	uint256 hash[34];	
 
 	sph_blake512_context     ctx_blake;
 	sph_bmw512_context       ctx_bmw;
@@ -41,58 +39,59 @@ void x13sm3_hash(const char* input, char* output, uint32_t len)
 	sph_fugue512_context     ctx_fugue;
 
 	sph_blake512_init(&ctx_blake);
-	sph_blake512(&ctx_blake, input, 80);
-	sph_blake512_close(&ctx_blake, hash);
+	sph_blake512(&ctx_blake, input, len);
+	sph_blake512_close(&ctx_blake, hash[0]);
 
 	sph_bmw512_init(&ctx_bmw);
-	sph_bmw512(&ctx_bmw, hash, 64);
-	sph_bmw512_close(&ctx_bmw, hash + 64);
+	sph_bmw512(&ctx_bmw, hash[0], 64);
+	sph_bmw512_close(&ctx_bmw, hash[2]);
 
 	sph_groestl512_init(&ctx_groestl);
-	sph_groestl512(&ctx_groestl, hash + 64, 64);
-	sph_groestl512_close(&ctx_groestl, hash);
+	sph_groestl512(&ctx_groestl, hash[2], 64);
+	sph_groestl512_close(&ctx_groestl, hash[4]);
 
 	sph_skein512_init(&ctx_skein);
-	sph_skein512(&ctx_skein, hash, 64);
-	sph_skein512_close(&ctx_skein, hash + 64);
+	sph_skein512(&ctx_skein, hash[4], 64);
+	sph_skein512_close(&ctx_skein, hash[6]);
 
 	sph_jh512_init(&ctx_jh);
-	sph_jh512(&ctx_jh, hash + 64, 64);
-	sph_jh512_close(&ctx_jh, hash);
+	sph_jh512(&ctx_jh, hash[6], 64);
+	sph_jh512_close(&ctx_jh, hash[8]);
 
 	sph_keccak512_init(&ctx_keccak);
-	sph_keccak512(&ctx_keccak, hash, 64);
-	sph_keccak512_close(&ctx_keccak, hash + 64);
+	sph_keccak512(&ctx_keccak, hash[8], 64);
+	sph_keccak512_close(&ctx_keccak, hash[10]);
 
-	memset(hash, 0, 64);	//sm3 is 256bit hash
+	hash[12].SetNull();	//sm3 is 256bit, just in case
+	hash[13].SetNull();
 	sm3_init(&ctx_sm3);
-	sph_sm3(&ctx_sm3, hash + 64, 64);
-	sph_sm3_close(&ctx_sm3, hash);
+	sph_sm3(&ctx_sm3, hash[10], 64);
+	sph_sm3_close(&ctx_sm3, hash[12]);
 
 	sph_cubehash512_init(&ctx_cubehash);
-	sph_cubehash512(&ctx_cubehash, hash, 64);
-	sph_cubehash512_close(&ctx_cubehash, hash + 64);
+	sph_cubehash512(&ctx_cubehash, hash[12], 64);
+	sph_cubehash512_close(&ctx_cubehash, hash[14]);
 
 	sph_shavite512_init(&ctx_shavite);
-	sph_shavite512(&ctx_shavite, hash + 64, 64);
-	sph_shavite512_close(&ctx_shavite, hash);
+	sph_shavite512(&ctx_shavite, hash[14], 64);
+	sph_shavite512_close(&ctx_shavite, hash[16]);
 
 	sph_simd512_init(&ctx_simd);
-	sph_simd512(&ctx_simd, hash, 64);
-	sph_simd512_close(&ctx_simd, hash + 64);
+	sph_simd512(&ctx_simd, hash[16], 64);
+	sph_simd512_close(&ctx_simd, hash[18]);
 
 	sph_echo512_init(&ctx_echo);
-	sph_echo512(&ctx_echo, hash + 64, 64);
-	sph_echo512_close(&ctx_echo, hash);
+	sph_echo512(&ctx_echo, hash[18], 64);
+	sph_echo512_close(&ctx_echo, hash[20]);
 
 	sph_hamsi512_init(&ctx_hamsi);
-	sph_hamsi512(&ctx_hamsi, hash, 64);
-	sph_hamsi512_close(&ctx_hamsi, hash + 64);
+	sph_hamsi512(&ctx_hamsi, hash[20], 64);
+	sph_hamsi512_close(&ctx_hamsi, hash[22]);
 
 	sph_fugue512_init(&ctx_fugue);
-	sph_fugue512(&ctx_fugue, hash + 64, 64);
-	sph_fugue512_close(&ctx_fugue, hash);
+	sph_fugue512(&ctx_fugue, hash[22], 64);
+	sph_fugue512_close(&ctx_fugue, hash[24]);
 
-	memcpy(output, hash, 32);
+	memcpy(output, hash, 24);
 
 }
